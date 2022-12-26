aefbaba53431626220de9be528be8e10f8d8925b
Mohamed Meabed <mo.meabed@gmail.com>
2022-12-25 19:36:31 -0500

    chore: improve deps

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testRegex: '.*.test.ts$',
-  testPathIgnorePatterns: ['<rootDir>/__tests__/__mocks__/', '<rootDir>/node_modules/', '<rootDir>/prod_node_modules/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testEnvironment: 'node',
-  collectCoverage: false,
-  coverageReporters: ['json', 'html'],
-  modulePathIgnorePatterns: ['<rootDir>/node_modules'],
+  testEnvironment: 'node'
 };
